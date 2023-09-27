c88a97d69cebc7a203180f97084bcdea3371b9a6
SnowArtz <ds.ortiz@uniandes.edu.co>
2023-09-26 19:11:11 -0500

    Finalización del reto

--- src/components/jobslist.js
+++ src/components/jobslist.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
-import Job from "./job";
+import {FormattedMessage} from 'react-intl';
 
-const JobsList = () => {
+import Job from "./job";
+import './styles.css'; // Asegúrate de que la ruta sea correcta
+
+
+const JobsList = ({ locale }) => {
+  const tableHeaderClass = locale.startsWith('es') ? 'thead-light' : 'thead-dark';
   const [offers] = useState([
     {
       id: "0001",
@@ -10,6 +15,7 @@ const JobsList = () => {
       salary: 4.5,
       city: "Bogotá, Colombia",
       date: "2019-03-26",
+      visites: 10000000,
     },
     {
       id: "0002",
@@ -18,6 +24,7 @@ const JobsList = () => {
       salary: 20,
       city: "Palo Alto, CA, USA",
       date: "2019-03-27",
+      visites: 1000,
     },
     {
       id: "0003",
@@ -26,20 +33,34 @@ const JobsList = () => {
       salary: 1,
       city: "Cali, Colombia",
       date: "2019-03-28",
+      visites: 1000,
     },
   ]);
 
   return (
     <div>
       <table className="table">
-        <thead className="thead-dark">
+        <thead className={tableHeaderClass}>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Position</th>
-            <th scope="col">Company</th>
-            <th scope="col">Salary</th>
-            <th scope="col">City</th>
-            <th scope="col">Publication date</th>
+            <th scope="col">
+              <FormattedMessage id="Position"/>
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Company"/>
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Salary"/>
+            </th>
+            <th scope="col">
+              <FormattedMessage id="City"/>
+            </th>
+            <th scope="col">
+              <FormattedMessage id="PublicationDate"/>
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Visits"/>
+            </th>
           </tr>
         </thead>
         <tbody>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const language = navigator.language;
+let locale, messages;
+
+console.log(language)
+
+if (language.startsWith("es")) {
+    locale = 'es-ES';
+    messages = localeEsMessages;
+} else if (language.startsWith("en")){
+    locale = 'en-US';
+    messages = localeEnMessages;
+} else{ 
+    locale = 'es-ES';
+    messages = localeEsMessages;
+}
+
+
+ReactDOM.render(
+    <IntlProvider locale={locale} messages={messages}>
+        <JobsList locale={locale}/>
+    </IntlProvider>, document.getElementById('root')
+);
